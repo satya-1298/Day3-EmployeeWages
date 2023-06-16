bb1b7008557d389dfea5a2dad88f5fa70ff2a24d
unknown <satya-1298>
2023-06-16 16:37:31 +0530

    [Satya] Added:Employee presence

---
+++ EmpWage/EmpAttendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    public class EmpAttendance
+    {
+        public void Check()
+        {
+            
+            Random random = new Random();
+            int attendent=random.Next(2);
+            Console.WriteLine(attendent);
+            if(attendent == 0) 
+            {
+                Console.WriteLine("Employee Absent");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee Present");
+
+            }
+        }
+    }
+}
