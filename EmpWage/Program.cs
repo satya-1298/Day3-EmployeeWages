fa3f5e49fa2868293d71f2058f0d05fe0cfe2bc8
unknown <satya-1298>
2023-06-16 16:32:38 +0530

    [Satya] Add : Greetings

---
+++ EmpWage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Program");
+            Console.ReadLine();
+        }
+    }
+}
